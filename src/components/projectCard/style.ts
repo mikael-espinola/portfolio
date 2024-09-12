92d2b3c1d342c1f8a12e3551065319ca2b660625
mikael-espinola <mikaelespinolaa@gmail.com>
2024-09-11 21:01:11 -0300

    teste

--- src/components/projectCard/style.ts
+++ src/components/projectCard/style.ts
@@ -5,7 +5,7 @@ import styled from "styled-components"
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    width: 25rem;
+    width: 100%;
     border: 2px solid gray;
     padding: 0.3rem;
     border-radius: 0.5rem;
