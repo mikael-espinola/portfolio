3c10e250483b538c0ee51a785d753b62383c64f1
mikael-espinola <mikaelespinolaa@gmail.com>
2025-03-01 18:22:49 -0300

    :sparkles: feat: reconstrução do formulário

--- src/backend/emailjs/index.ts
+++ src/backend/emailjs/index.ts
@@ -1,29 +1,45 @@
-import emailjs from 'emailjs-com'
+import emailjs from "emailjs-com";
 
 type prop = {
-  name: string, email: string, whatsapp: string, message: string
-}
+  name: string;
+  email: string;
+  whatsapp: string;
+  message: string;
+  setIsSending: (value: boolean) => void;
+  setStatus: (value: string) => void;
+};
 
-const sendEmail = ({name, email, whatsapp, message}: prop) => {
-    const publicKey = process.env.NEXT_PUBLIC_PUBLIC_EMAIL_KEY
-    const serviceId =  process.env.NEXT_PUBLIC_SERVICE_EMAIL_ID
-    const templateId = process.env.NEXT_PUBLIC_TEMPLATE_EMAIL_ID
-    const templateParams = {
-      name: name,
-      email: email,
-      whatsapp: whatsapp,
-      message: message,
+const sendEmail = ({
+  name,
+  email,
+  whatsapp,
+  message,
+  setIsSending,
+  setStatus,
+}: prop) => {
+  const publicKey = process.env.NEXT_PUBLIC_PUBLIC_EMAIL_KEY;
+  const serviceId = process.env.NEXT_PUBLIC_SERVICE_EMAIL_ID;
+  const templateId = process.env.NEXT_PUBLIC_TEMPLATE_EMAIL_ID;
+  const templateParams = {
+    name: name,
+    email: email,
+    whatsapp: whatsapp,
+    message: message,
   };
 
-  if(publicKey && serviceId && templateId){
-    
+  setIsSending(true);
+  if (publicKey && serviceId && templateId) {
     emailjs
-    .send(serviceId, templateId, templateParams, publicKey)
-    
-    .catch((error) => {
-      console.error("Error sending email:", error);
-    });
-  }
-}
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((result) => {
+        setIsSending(false);
+        setStatus(result.text);
+      })
 
-export default sendEmail
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+  }
+};
+
+export default sendEmail;
--- src/components/formContact/style.tsx
+++ src/components/formContact/style.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface Props {
+  $status?: boolean;
+}
+
 export const Container = styled.div`
   display: flex;
   align-items: center;
@@ -37,33 +41,66 @@ export const Input = styled.input`
   }
 `;
 
-export const MessageField = styled.textarea`
-  width: 100%;
-  height: 6rem;
-  padding: 0.6rem 0.5rem 0;
-  font-family: "Segoe UI";
-  border-radius: 8px;
-  border: none;
-
-  &:focus {
-    outline: 2px solid orange;
-  }
-`;
-
 export const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 0.6rem;
 `;
 
-export const Button = styled.button`
+export const Button = styled.button<Props>`
   display: flex;
   width: max-content;
   padding: 1rem;
   cursor: pointer;
   border-radius: 8px;
   border: none;
-  background-color: #ffaa00;
+  background-color: ${({ $status }) => ($status ? "#00ff00" : "#ffaa00")};
   color: black;
   font-weight: 600;
+`;
+
+export const InputWrapper = styled.div`
+  position: relative;
+  margin: 0.2rem 0;
+`;
+
+export const InputItem = styled.input`
+  border-radius: 6px;
+  width: 100%;
+  padding: 1rem;
+  font-size: 16px;
+  border: 1px solid #ffffff;
+  color: #ffffff;
+  outline: none;
+  background: transparent;
+
+  &:focus {
+    outline: 0.1rem solid #ffaa00;
+    outline-offset: 2px;
+  }
+
+  &:focus + label,
+  &:valid + label {
+    top: 0;
+    font-size: 0.875rem;
+    background-color: #0d133e;
+    border-radius: 6px;
+    padding: 0 0.375rem;
+  }
+`;
+
+export const LabelItem = styled.label`
+  position: absolute;
+  top: 50%;
+  left: 1rem;
+  transform: translateY(-50%);
+  color: #d1d1d1;
+  font-size: 1rem;
+  pointer-events: none;
+  transition: 0.3s;
+`;
+
+export const MessageField = styled(InputItem).attrs({ as: "textarea" })`
+  min-height: 4rem;
+  max-width: 100%;
 `;
